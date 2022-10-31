e95456a187c34a3a75120df124ffd4ab7a1e80e4
Nico <nicoc12024@gmail.com>
2022-11-01 00:20:32 +0100

    updated

--- pages/movie-data-base.jsx
+++ pages/movie-data-base.jsx
@@ -17,7 +17,7 @@ function MovieDataBase() {
         />
         <div className="absolute px-8 top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
           <h2 className="py-2">Movie Data Base</h2>
-          <h3> React JS / JavaScript / CSS / Firebase</h3>
+          <h3> React JS / JavaScript / CSS </h3>
         </div>
       </div>
 
