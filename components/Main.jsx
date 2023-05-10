0529416aa4c5fabaaf823e37c21a7ce26bac3b4b
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2023-05-11 01:11:54 +0200

    m

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,11 +10,11 @@ function Main() {
             LET'S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I'm <span className="text-[#5651e5]"> Nicolas</span>
+            Hi, I'm <span className="text-[#5651e5]">Nicolas</span>
           </h1>
-          <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
+          <h1 className="py-2 text-gray-700">A Software Developer</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-            I'm focused on building responsive front-end web applications.
+            I'm dedicated to building innovative solutions with a modern technology stack.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
